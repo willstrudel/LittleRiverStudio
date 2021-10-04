dfb3d0709fb9bdd8c0e870cf9e8ae3a80621b373
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-04 16:39:19 -0400

    Changed homepage message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TESTING GH PAGES WITH REACT AND GH ACTIONS
+          <h1>TESTING GH PAGES WITH REACT AND GH ACTIONS</h1>
         </p>
         <a
           className="App-link"
