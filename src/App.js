8a4c88d4d83da936e57ffca7f9226bc47c339908
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-04 18:21:10 -0400

    created files for homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
           <h1>Welcome</h1>
-        </p>
-       
-      </header>
     </div>
   );
 }
