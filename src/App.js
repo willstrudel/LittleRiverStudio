736445367f21e72f682ceba869a1c97c16bfb1a9
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-04 18:07:01 -0400

    Updated text on homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Beeb</h1>
+          <h1>Welcome</h1>
         </p>
        
       </header>
