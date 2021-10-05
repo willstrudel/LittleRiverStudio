b1181659463fe05b7ae15a0a0f426784fec1ff82
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-04 23:54:00 -0400

    Updated Project button on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className="bg-gray-800 h-screen flex flex-col justify-center items-center">
             <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl uppercase font-black mb-14 text-white">Welcome</h1>
-            <Link to="/projects" className="mt-12 py-5 px-8 bg-black rounded-full text-3xl hover:bg-gray-600 transition duration-300 ease-in-out flex items-center animate-bounce text-white">Projects</Link>
+            <Link to="/projects" className="mt-12 py-4 px-6 bg-black rounded-full sm:text-1xl md:text-2xl lg:text-3xl hover:bg-gray-600 lg:transition lg:duration-300 lg:ease-in-out flex items-center animate-bounce text-white font-quicksand">Projects</Link>
         </div>
     )
 }
