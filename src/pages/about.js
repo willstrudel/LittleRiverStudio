fad5aa5b7d3450d7eda365549d0eed00c877365c
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-05 22:14:52 -0400

    Positioned and styled headhsot image and about me, working on responsiveness

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,12 @@ import Headshot from '../images/headshot.JPG'
 
 const About = () => {
     return (
-        <div className="h-screen flex justify-center items-center bg-gray-500">
-            <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl  uppercase font-black flex relative">About Me</h1>  
-            <img src={Headshot} alt="headshot" className="justify-center flex absolute items-center box-content lg:h-96 lg:w-84 p-4 border-2 rounded shadow-2xl"/>          
+        <div className="h-screen lg:grid bg-gray-500">
+            <div className="relative h-64 w-26">
+                <h1 className="absolute flex xl:left-56 lg:top-28 sm:items-center sm:inset-x-0 sm:inset-y-4 lg:text-8xl md:text-7xl sm:text-6xl text-5xl uppercase font-black">About Me</h1>  
+            </div>
+                <img className="absolute inset-y-36 lg:right-12 sm:h-3/4 w-auto rounded-2xl border-white border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />          
+            
         </div>
     )
 }
