1b815510f1f70e25b6eeb5b74e5dcf178f78868a
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-06 22:37:20 -0400

    Updated mobile responsive About Me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,12 @@ import Headshot from '../images/headshot.JPG'
 
 const About = () => {
     return (
-        <div className="h-screen bg-gray-500 grid grid-flow ">
+        <div className="xs:h-screen xs:w-screen bg-gray-500 grid grid-flow ">
             <div>
                 <h1 className="
                     relative 
                     grid underline left-0 top-20 items-center justify-center text-4xl uppercase font-black
+                    xs:-left-1
                     sm:left-40 sm:top-24 sm:text-6xl
                     md:left-56 md:top-24 md:text-7xl
                     lg:top-24 lg:left-56
@@ -17,14 +18,16 @@ const About = () => {
                 <p className="
                 absolute grid
                 left-12 bottom-16 font-quicksand w-96 text-black text-center text-lg
+                xs:h-60 xs:w-72 xs:text-base xs:break-words xs:bottom-24 xs:left-10
                 sm:left-36
                 md:inset-x-64  
                 lg:text-2xl"
                 >Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase exposure. My personal hobbies include playing music, cooking, and hanging out with my dog.
                 </p>
                     <img className="
-                    absolute grid
+                    relative grid
                     h-80 inset-y-56 inset-x-14 w-auto rounded-2xl border-white border-2 shadow-2xl p-2
+                    xs:h-56 xs:-top-36 xs:left-14
                     sm:flex sm:absolute sm:h-1/2 sm:left-12 sm:right-36 
                     md:flex md:absolute 
                     lg:flex lg:right-96 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xs': '375px',
+      'xl': '1630px',
+    },
     objectPosition: {
       'center-top': 'center top',
     },
     extend: {
-      screens: {
-        'xs': '500px',
-        'xl': '1630px',
-      },
       fontFamily: {
         'quicksand': ["Quicksand", "sans-serif"],
       },
