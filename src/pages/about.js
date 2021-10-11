88c9dbf6c89daaf109d533a295d3d9dc30150a55
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-10 23:18:36 -0400

    Updated flexbox design on About Me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,40 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react'
 import Headshot from '../images/headshot.JPG'
 
 const About = () => {
     return (
-        <div className="xs:h-screen xs:w-screen bg-gray-500 grid grid-flow ">
-            <div>
-                <h1 className="
-                    relative 
-                    grid underline left-0 top-20 items-center justify-center text-4xl uppercase font-black
-                    xs:-left-1
-                    sm:left-40 sm:top-24 sm:text-6xl
-                    md:left-56 md:top-24 md:text-7xl
-                    lg:top-24 lg:left-56
-                    xl:w-96 xl:left-56 xl:top-28 xl:text-8xl"
-                        >About Me</h1>  
+            <>
+            <main className="px-12 h-screen w-screen bg-gray-500 ">
+
+                <div className="flex justify-center relative">
+                    <div className="top-24 absolute underline text-4xl uppercase font-black">
+                        <h1>About Me</h1> 
+                    </div>
+
+                    <div className="relative flex justify-center">
+                        
+                    <img className="absolute top-60 max-w-sm h-80 rounded-md border-white border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />
+                       
+                    </div>
+                </div>
+            </main>
+            
+            <div className="relative flex justify-center">
+            <p className="absolute bottom-20 w-72 font-quicksand text-black text-center text-md">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase exposure. My personal hobbies include playing music, cooking, and hanging out with my dog.
+            </p>
             </div>
-                <p className="
-                absolute grid
-                left-12 bottom-16 font-quicksand w-96 text-black text-center text-lg
-                xs:h-36 xs:w-72 xs:text-base xs:break-words xs:bottom-52 xs:left-24
-                sm:left-36
-                md:inset-x-64
-                lg:text-2xl"
-                >Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase exposure. My personal hobbies include playing music, cooking, and hanging out with my dog.
-                </p>
-                    <img className="
-                    relative grid
-                    h-80 inset-y-56 inset-x-14 w-auto rounded-2xl border-white border-2 shadow-2xl p-2
-                    xs:h-56 xs:-top-36 xs:left-14
-                    sm:flex sm:absolute sm:h-1/4 sm:left-28  
-                    md:flex md:absolute 
-                    lg:flex lg:right-96 
-                    xl:inset-y-56 xl:inset-x-96" 
-                    src={Headshot} 
-                    alt="headshot" />          
-             </div>
+            </>
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,15 +3,22 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="h-screen flex justify-center items-center bg-gray-500">
-            <h1 className="absolute flex object-center-top xl:text-8xl lg:text-6xl md:text-4xl sm:text-3xl text-2xl uppercase font-black">Contact</h1>
+            <h1 className="
+                absolute flex object-center-top uppercase font-black
+                xl:text-8xl 
+                lg:text-6xl 
+                md:text-4xl 
+                sm:text-3xl 
+                text-2xl ">Contact</h1>
+
             <div className="relative text-center text-2xl top-52">
-             <ul>
-                 <li>Email</li>
-                 <li>LinkedIn</li>
-                 <li>GitHub</li>
-                 <li>YouTube</li>
-                 <li>Instagram</li>
-             </ul>
+                <ul>
+                    <li>Email</li>
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                    <li>YouTube</li>
+                    <li>Instagram</li>
+                </ul>
             </div>            
         </div>
     )
