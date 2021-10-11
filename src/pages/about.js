8ae72bebedba973294cda4fc41dfc1d8fdc49e87
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-11 00:33:06 -0400

    Updated mobile layout for About Me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,14 +13,14 @@ const About = () => {
 
                     <div className="relative flex justify-center">
                         
-                    <img className="absolute top-60 max-w-lg h-80 rounded-sm border-white border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />
+                    <img className="absolute top-60 max-w-lg w-60 h-52 rounded-sm border-white border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />
                        
                     </div>
                 </div>
             </main>
             
             <div className="relative flex flex-wrap justify-center">
-                <p className="absolute bottom-10 w-72 font-quicksand text-black text-center text-sm">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
+                <p className="absolute bottom-14 w-72 font-quicksand text-black text-center text-sm">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
                 </p>
             </div>
             </>
