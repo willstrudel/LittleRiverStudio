3da7c69e70592bfac8a3a766b5defbc99a6a63c6
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-15 19:37:09 -0400

    Updated styling on ipad About Me

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,12 +13,12 @@ const About = () => {
                         <h1>About Me</h1> 
                     </div>
                     <div className="relative flex justify-center"> 
-                        <img className="absolute 8plus:top-28 xs:text-xs xs:top-32 xs:w-auto xs:h-40 sm:top-40 md:top-60 max-w-lg w-60 h-52 rounded-md border-white border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />
+                        <img className="absolute 8plus:top-28 xs:text-xs xs:top-32 xs:w-auto xs:h-40 sm:top-44 md:top-72 md:w-auto md:h-72 max-w-lg w-60 h-52 rounded-md border-white border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />
                     </div>
                 </div>
             </main>
             <div className="relative flex flex-wrap justify-center">
-                <p className="absolute 8plus:bottom-2 xs:bottom-8 xs:text-sm sm:bottom-24 sm:text-base md:bottom-32 w-72 font-quicksand text-black text-center text-md">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and site architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
+                <p className="absolute 8plus:bottom-2 xs:bottom-8 xs:text-sm sm:bottom-24 sm:text-base md:bottom-48 md:text-2xl md:w-screen md:px-36 w-72 font-quicksand text-black text-center text-md">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and site architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
                 </p>
             </div>
             </>
