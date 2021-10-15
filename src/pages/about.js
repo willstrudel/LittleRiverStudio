6737e823c32bce12f5f361b08e69a16f41b1a1eb
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-15 17:02:22 -0400

    Updated responsive breakpoints on About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const About = () => {
             <>
             <main className="px-12 h-screen w-screen bg-gray-500 ">
                 <div className="flex flex-wrap justify-center relative">
-                    <div className="top-24 xs:top-16 absolute underline text-4xl uppercase font-black">
+                    <div className="top-24 xs:top-16 sm-top-12 absolute underline text-4xl uppercase font-black">
                         <h1>About Me</h1> 
                     </div>
                     <div className="relative flex justify-center"> 
@@ -18,7 +18,7 @@ const About = () => {
                 </div>
             </main>
             <div className="relative flex flex-wrap justify-center">
-                <p className="absolute bottom-14 w-72 font-quicksand text-black text-center text-md">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and site architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
+                <p className="absolute xs:bottom-8 text-sm w-72 font-quicksand text-black text-center text-md">Hey, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp. My strengths lie in Front End design and site architecture. When I'm not building React websites and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
                 </p>
             </div>
             </>
