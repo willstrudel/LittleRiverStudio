689da59cc0e71f3fe4482bb34ca4f25973e3c2ea
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-20 14:28:06 -0400

    Updated mobile responsiveness to home/hero and about me pages

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,31 +4,38 @@ import River from '../images/river.jpg';
 
 const Hero = () => {
     return (
-        <div className="min-h-screen p-8 bg-gray-600 sm:grid sm:grid-cols-1 justify-center">
-            <h1 className="justify-center flex uppercase font-black mt-14 text-white
+        <div className="min-h-screen p-4 bg-gray-600 grid grid-cols-1">
+            
+            <h1 className="justify-center flex uppercase font-black mt-20 text-white
                 lg:text-8xl 
                 md:text-7xl 
                 sm:text-5xl
-                text-5xl">Welcome</h1>
+                text-5xl">Welcome
+            </h1>
 
             <p className="text-white justify-center flex font-dosis 
-                text-xl mt-6
+                text-xl
                 md:text-3xl
-                lg:text-5xl">to</p>
+                lg:text-5xl">to
+            </p>
 
             <p className="text-white font-dosis justify-center flex 
                 text-3xl 
-                md:text-4xl m-3 mb-6
-                lg:text-5xl">Little River Studio</p>
+                md:text-4xl
+                lg:text-5xl">Little River Studio
+            </p>
 
-            <img className=" bg-cover rounded-xl" src={River} alt="river-img" />
-                <Link className="flex justify-center" to="/about">
-                <div className="uppercase mt-12 px-4 py-4 text-center items-center bg-black rounded-full flex justify-center animate-bounce text-white font-quicksand hover:bg-blue-500 
-                text-sm sm:hidden
+        <div className="flex justify-center">
+            <img className="w-full max-w-7xl rounded-xl" src={River} alt="river-img" />
+        </div>
+             <Link className="flex justify-center p-16" to="/about">
+                <div className="uppercase mt-4 px-3 h-12 text-center items-center bg-black rounded-lg flex justify-center animate-bounce text-white font-quicksand hover:bg-green-700 
+                text-sm md:hidden
                 md:text-1xl  
                 lg:text-2xl 
-                transition duration-300 ease-in-out">About Me</div>
-                </Link>
+                transition duration-300 ease-in-out">About Me
+                </div>
+            </Link>
         </div>
     )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,14 +7,14 @@ ReactModal.setAppElement('#root');
 const About = () => {
     return (
         <>
-            <main className="min-h-screen p-8 bg-gray-200 sm:grid sm:grid-cols-2 justify-center">
+            <main className="min-h-screen p-8 bg-gray-400 sm:grid sm:grid-cols-2 justify-center">
                 
-                <div className="bg-red-300 justify-center xl:justify-start pt-10 lg:mt-0 lg:pt-10">
+                <div className="justify-center xl:justify-start pt-16 lg:mt-0 lg:pt-10">
                     <h1 className="justify-center mb-10 flex text-5xl md:text-7xl uppercase font-black">About Me</h1>
-                        <div>
-                        <p className="bg-blue-300 font-quicksand text-black text-center text-sm flex font-bold
+                        <div className="flex justify">
+                        <p className="font-quicksand text-black text-center text-sm flex font-bold
                             mb-10 flex-col  
-                            md:text-xl sm:mt-0 sm:mb-0 sm:justify-center
+                            md:text-xl sm:mt-0 sm:mb-0 justify-center
                             lg:text-2xl
                             xl:justify-start">
                          Hey there, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp, specializing in Front End design and site architecture using Tailwind CSS and React. When I'm not building projects and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
@@ -23,9 +23,9 @@ const About = () => {
                        
                 </div>
 
-                <div className="bg-indigo-200 sm:flex">
-                    <div className="bg-yellow-300 sm:flex sm:flex-col sm:justify-center"> 
-                        <img className="rounded-md border-yellow-50 border-2 shadow-2xl p-2" src={Headshot} alt="headshot" />
+                <div className="flex">
+                    <div className="flex flex-col justify-center"> 
+                        <img className="rounded-xl border-gray-300 border-4 shadow-2xl" src={Headshot} alt="headshot" />
                     </div> 
                 </div>     
 
