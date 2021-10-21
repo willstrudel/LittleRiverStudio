0b0ebe3e233befde57419ef9f44dd027ae843601
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-21 17:52:24 -0400

    Updated positioning/styling of About Me page, updated project links on Projects page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,24 +7,25 @@ ReactModal.setAppElement('#root');
 const About = () => {
     return (
         <>
-            <main className="min-h-screen p-8 bg-gray-400 sm:grid sm:grid-cols-2 justify-center">
+            <main className="min-h-screen p-8 bg-gray-300 grid sm:grid-cols-2 justify-center items-center">
                 
-                <div className="justify-center xl:justify-start pt-16 lg:mt-0 lg:pt-10">
-                    <h1 className="justify-center mb-10 flex text-5xl md:text-7xl uppercase font-black">About Me
+                <div className="gap-24 grid justify-center mt-14">
+                    <h1 className="justify-center flex text-5xl md:text-7xl uppercase font-black">About Me
                     </h1>
-                        <div className="flex justify">
-                            <p className="font-quicksand text-black text-center text-sm flex font-bold
-                                mb-10 flex-col  
-                                md:text-xl sm:mt-0 sm:mb-0 justify-center
-                                lg:text-2xl
-                                xl:justify-start">
+                        <div className="grid">
+                            <p className="font-quicksand text-black text-center text-sm font-bold
+                                flex-col leading-loose
+                                sm:text-lg  
+                                md:text-xl
+                                lg:text-2xl lg:leading-8
+                                xl:leading-10">
                             Hey there, my name is Will Struder. I am a recent graduate of the Georgia Tech Full-Stack Coding Bootcamp, specializing in Front End design and site architecture using Tailwind CSS and React. When I'm not building projects and finding new challenges in tech, I shoot videos for local businesses to increase brand awareness. My personal hobbies include playing music, cooking, and hanging out with my dog.
                             </p>
                         </div>     
                 </div>
-                <div className="flex">
+                <div className="p-12">
                     <div className="flex flex-col justify-center"> 
-                        <img className="rounded-xl border-gray-300 border-4 shadow-2xl" src={Headshot} alt="headshot" />
+                        <img className="rounded-xl border-indigo-500 border-4 shadow-3xl" src={Headshot} alt="headshot" />
                     </div> 
                 </div>     
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom';
 
 const Projects = () => {
     return (
-        <div className="min-h-screen justify-center items-center bg-indigo-900 grid grid-col">
-            <div className="flex justify-center">
-                <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-6xl uppercase text-white font-black">Projects</h1>
-            </div>
-
-            <div className="bg-gray-300 lg:grid lg:grid-cols-3 lg:space-x-6 justify-center">
-                
-                    <div className="bg-green-300 flex justify-center uppercase text-2xl lg:text-4xl">
-                        <Link className="flex justify-center" to="/videos">
-                            <p>Videos</p>
-                        </Link> 
-                    </div>
-                    <div className="bg-red-300 flex justify-center uppercase text-2xl lg:text-4xl">
-                        <Link className="flex justify-center" to="/photography">
-                            <p>Photography</p>
-                        </Link> 
-                    </div>
-                    <div className="bg-yellow-300 flex justify-center uppercase text-2xl lg:text-4xl">
-                        <Link className="flex justify-center" to="/coding">
-                            <p>Coding</p>
-                        </Link> 
-                    </div>
+        <div className="min-h-screen grid justify-center items-center bg-indigo-900 ">
+               <div className="flex justify-center">
+                    <h1 className="flex justify-center uppercase font-black text-white text-6xl shadow-2xl
+                lg:text-7xl 
+                md:text-7xl">Projects</h1>
+                </div>
+            <div className=" justify-center grid gap-6 sm:grid-cols-3 lg:grid-cols-3">
+             
+                        
+                <div className="flex items-center justify-center text-white hover:scale-125 hover:-translate-y-2 transform-gpu uppercase font-black text-2xl lg:text-4xl">
+                    <Link className="flex justify-center" to="/videos">
+                        <p>Videos</p>
+                    </Link> 
+                </div>
+                <div className="flex items-center justify-center text-white hover:scale-125 hover:-translate-y-2 transform-gpu uppercase font-black text-2xl lg:text-4xl">
+                    <Link className="flex justify-center" to="/photography">
+                        <p>Photography</p>
+                    </Link> 
+                </div>
+                <div className="flex items-center justify-center text-white hover:scale-125 hover:-translate-y-2 transform-gpu uppercase font-black text-2xl lg:text-4xl">
+                    <Link className="flex justify-center" to="/coding">
+                        <p>Coding</p>
+                    </Link> 
+                </div>
             </div>          
         </div>
     )
