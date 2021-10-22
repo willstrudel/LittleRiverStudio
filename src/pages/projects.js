d7aaef06093a4d8bb38fb82e53f2573f22f5f32b
Will Struder <80079439+willstrudel@users.noreply.github.com>
2021-10-22 17:40:08 -0400

    Added videos with links in description, restyled Projects Page also

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,29 +8,29 @@ const Projects = () => {
     return (
         <div className="min-h-screen grid justify-center items-center bg-indigo-900">
              
-            <div className="flex justify-center sm:mt-20 md:mt-10 lg:mt-20 mt-20 border-b-2 border-white xl:mb-2 sm:border-none">
-                <h1 className="xl:mt-10 mb-8 flex justify-center uppercase font-black text-white text-6xl shadow-2xl md:text-7xl lg:text-7xl">Projects</h1>
+            <div className="flex justify-center sm:mt-20 md:mt-10 lg:mt-20 mt-20 border-b-2 border-white xl:mb-10 lg:border-none">
+                <h1 className="md:mt-10 mb-8 lg:mb-0 flex justify-center uppercase font-black text-white text-6xl shadow-2xl md:text-7xl lg:text-7xl">Projects</h1>
             </div>
 
 
-            <div className="m-20 grid grid-rows-1 gap-7 sm:gap-x-6 sm:grid-rows-1 md:grid-rows-2 md:grid-flow-col lg:grid-cols-3 ">
+            <div className="items-center m-14 md:m-12 grid grid-rows-1 gap-y-10 sm:gap-y-8 sm:gap-x-8 md:gap-x-10 lg:gap-x-12 xl:gap-x-16 sm:grid-rows-1 lg:grid-rows-2 lg:grid-flow-col lg:grid-cols-3">
 
-                <img className="rounded-xl shadow-xl" src={YTtacos} alt="Taco video" />
-                <div className="flex items-center justify-center text-white hover:scale-125 hover:-translate-y-2 transform-gpu uppercase font-black text-2xl md:text-3xl lg:text-4xl">
+                <img className="rounded-xl shadow-2xl" src={YTtacos} alt="Taco video" />
+                <div className="flex items-center justify-center">
                     <Link className="flex justify-center" to="/videos">
-                        <p>Videos</p>
+                        <p className="text-white uppercase font-black text-2xl md:text-3xl lg:text-4xl hover:scale-125 hover:-translate-y-2 transform-gpu">Videos</p>
                     </Link> 
                 </div>
-                <img className="rounded-xl shadow-xl" src={Vans} alt="Vans shoes" />
-                <div className="flex items-center justify-center text-white hover:scale-125 hover:-translate-y-2 transform-gpu uppercase font-black text-2xl md:text-3xl lg:text-4xl">
+                <img className="rounded-xl shadow-2xl" src={Vans} alt="Vans shoes" />
+                <div className="flex items-center justify-center">
                     <Link className="flex justify-center" to="/photography">
-                        <p>Photography</p>
+                        <p className="text-white uppercase font-black text-2xl md:text-3xl lg:text-4xl hover:scale-125 hover:-translate-y-2 transform-gpu">Photography</p>
                     </Link> 
                 </div>
-                <img className="rounded-xl shadow-xl" src={enCapsulate} alt="enCapsulate" />
-                <div className="flex items-center justify-center text-white hover:scale-125 hover:-translate-y-2 transform-gpu uppercase font-black text-2xl md:text-3xl lg:text-4xl">
+                <img className="rounded-xl shadow-2xl" src={enCapsulate} alt="enCapsulate" />
+                <div className="flex items-center justify-center">
                     <Link className="flex justify-center" to="/coding"> 
-                        <p>Coding</p>
+                        <p className="text-white uppercase font-black text-2xl md:text-3xl lg:text-4xl hover:scale-125 hover:-translate-y-2 transform-gpu">Coding</p>
                     </Link> 
                 </div>
 
--- src/pages/videos.js
+++ src/pages/videos.js
@@ -3,18 +3,35 @@ import React from 'react'
 const Videos = () => {
     return (
         <>
-            <main className="min-h-screen justify-center grid grid-cols-1 bg-gray-500">
-                <div className="bg-red-300 pt-28">
-                        <h1 className="justify-center grid text-5xl md:text-7xl uppercase font-black">Videos</h1>
-                    <div className="bg-blue-300 grid  pt-28">
-                        <div class="grid aspect-w-16 aspect-h-12 m-8 ">
-                            <iframe title="Corner Street Tacos" src="https://www.youtube.com/embed/UBOj6rqRUME" frameBorder="0" className="grid" allowFullScreen></iframe>
-                        </div>
-                    </div>
+            <main className="min-h-screen justify-center items-center bg-blue-300">
+                <div className="flex justify-center border-b-4 border-indigo-500 xl:mb-2 sm:border-none">
+                     <h1 className=" mt-28 flex justify-center uppercase font-black text-black text-6xl md:text-7xl lg:text-7xl">Videos</h1>
                 </div>
 
-               
 
+                <div className=" bg-blue-300 items-center grid grid-rows-1 md:grid-row-1 lg:grid-cols-2">
+
+                  
+                    <div className="grid md:pt-0 pt-10 sm:pt-0">
+                        <div className="flex items-center justify-center m-8 md:m-12 lg:m-16 xl:m-20 aspect-w-16 aspect-h-10">
+                            <iframe title="Corner Street Tacos" src="https://www.youtube.com/embed/jYvXHDoa_3g" className="flex rounded-lg border-2 shadow-xl" allowFullScreen></iframe>
+                        </div>
+                        <h4 className="flex justify-start lg:justify-center text-black ml-8 md:ml-12 lg:ml-0 uppercase font-black text-3xl md:text-4xl lg:text-5xl">Corner Street
+                        </h4>
+                        <p className="m-8 md:m-12 lg:m-16 xl:m-20 text-center text-black text-xl md:text-2xl font-dosis">First video shoot for local restaurant <a className=" text-indigo-800 visited:text-indigo-800 hover:scale-125 hover:-translate-y-2 transform-gpu" href="https://www.ordercornerstreet.com/" target="_blank" rel="noopener noreferrer">The Corner Street Taco Bar</a>, such a wonderful experience with incredible employees taking care of us!
+                        </p>
+                    </div>
+
+                    <div className="grid">
+                        <div class="flex items-center justify-center m-8 md:m-12 lg:m-16 xl:m-20 aspect-w-16 aspect-h-10">
+                            <iframe title="Corner Street Tacos" src="https://www.youtube.com/embed/skSXAAl0rgY" className="flex rounded-lg border-2 shadow-xl" allowFullScreen></iframe>
+                        </div>
+                        <h4 className="flex justify-end lg:justify-center text-black mr-8 md:mr-12 lg:mr-0 uppercase font-black text-3xl md:text-4xl lg:text-5xl">Fall Festival
+                        </h4>
+                        <p className="m-8 md:m-12 lg:m-16 xl:m-20 text-center text-xl md:text-2xl font-dosis">This video was made while visiting the <a className=" text-indigo-800 visited:text-indigo-800 hover:scale-125 hover:-translate-y-2 transform-gpu" href="https://visitwoodstockga.com/landing-page/autumn-in-woodstock-ga/" target="_blank" rel="noopener noreferrer">Woodstock Georgia Fall Festival</a> in late September, looking forward to many more this year!
+                        </p>
+                    </div>
+                </div>
             </main>
         </>
     )
